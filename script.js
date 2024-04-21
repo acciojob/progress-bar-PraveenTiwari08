6167788ea00581f0fdbbf4df446e04db5af913ab
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-21 18:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll(".circle");
+const lines = document.querySelectorAll(".line");
+const nextbutton = document.querySelector("#next");
+const previousbutton = document.querySelector("#prev");
+
+let currentActive = 1;
+
+function previous() {
+	currentActive--;
+	if(currentActive < 1){
+		currentActive = 1;
+	}
+	ui_Update();
+}
+
+function next() {
+	currentActive++;
+	if(currentActive > circles.length){
+		currentActive = circles.length;
+	}
+	ui_Update();
+}
+
+function ui_Update() {
+	circles.forEach((circle,index)=>{
+		if(index < currentActive){
+			circle.classList.add("active");
+		}else{
+			circle.classList.remove("active");
+		}
+	})
+}
